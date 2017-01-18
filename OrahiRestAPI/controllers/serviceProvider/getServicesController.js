519a814bb2edb448b476d5cb4344c0b5f8f6fc2a
Male Michael <malmike21@outlook.com>
2017-01-19 00:11:56 +0300

    Made changes to the return service of the user, such that the user gets the entire category object rather that getting a reference id

--- OrahiRestAPI/controllers/serviceProvider/getServicesController.js
+++ OrahiRestAPI/controllers/serviceProvider/getServicesController.js
@@ -1,21 +1,43 @@
 ﻿var responses = require("../responses")();
+var category = require("../../models/categoryModel")();
 
 var getController = function (Value)
 {
-    var get = function ( req, res )
-    {
+    var get = function (req, res) {
         var query = {};
-        if ( req.query.serviceType )
-        {
+        if (req.query.serviceType) {
             query.serviceType = req.query.serviceType;
         }
         if (req.query.category) {
             query.category = req.query.category;
         }
-        if ( req.decoded._doc._id )
-        {
+        if (req.decoded._doc._id) {
             query.serviceProvider = req.decoded._doc._id;
         }
+
+        //Value.aggregate([
+        //    {
+        //        $lookup:
+        //        {
+        //            from: "category",
+        //            localField: "category",
+        //            foreignField: "_id",
+        //            as: "category"
+        //        }
+        //    },
+        //    { $unwind: "$category" },
+        //    { $match: query }
+        //], function (err, value) {
+        //    if (err)
+        //    {
+        //        responses.failureOutput(req, res, err);
+        //    }
+        //    else {
+        //        responses.successfulOutput(req, res, value);
+        //    }
+        //});
+
+            
         Value.find(query, function ( err, value )
         {
             if ( err )
---
+++ OrahiRestAPI/controllers/user/getServicesController.js
@@ -0,0 +1,41 @@
+﻿var responses = require("../responses")();
+var mongoose = require("mongoose");
+
+var getController = function (Value) {
+    var get = function (req, res) {
+
+        var query = {};
+        if (req.query.serviceProvider) {
+            query.serviceProvider = req.query.serviceProvider;
+
+            Value.aggregate([
+                {
+                    $lookup:
+                    {
+                        from: "categories",
+                        localField: "category",
+                        foreignField: "_id",
+                        as: "category"
+                    }
+                },
+                { $unwind: { path: "$category", preserveNullAndEmptyArrays: true } },
+                { $match: { serviceProvider: mongoose.Types.ObjectId(query.serviceProvider) } }
+            ], function (err, value) {
+                if (err) {
+                    responses.failureOutput(req, res, err);
+                }
+                else{
+                    responses.successfulOutput(req, res, value);
+                    }
+                }
+            );
+        } else {
+            responses.failureOutput(req, res, "No service provider given");
+        }
+    }
+    return {
+        get: get
+    }
+}
+
+module.exports = getController;
