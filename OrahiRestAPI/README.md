7360e6ba0b7f31f260867815b315c1654f5a2a71
nfatah <nfatah41@gmail.com>
2017-09-04 21:52:05 +0300

    Set up swagger & update dependencies/READ ME

--- OrahiRestAPI/README.md
+++ OrahiRestAPI/README.md
@@ -1,3 +1,10 @@
 ﻿# OrahiRestAPI
 
+This is the Orahi Restul API that feeds both Orahi android and web clients
 
+## start the server 
+run `npm start` to start server
+
+## Access Documentation
+1. visit `localhost:1337/api-docs`
+2. Add more specs to swa
--- OrahiRestAPI/server.js
+++ OrahiRestAPI/server.js
@@ -8,15 +8,46 @@
     config = require( './config' ),
     User = require( './models/userModel' ),
     path = require( 'path' ),
-    fs = require('fs');
+    fs = require('fs'),
+    swaggerJSDoc = require('swagger-jsdoc');
+
 
 var environment = process.env.ENV;
 var db = mongoose.connect( config.database ); //connect to the database
+
+/*********************************
+* swagger spec definition
+*/
+var swaggerDefinition = {
+  info: {
+    title: 'Orahi API',
+    version: '1.0.0',
+    description: 'Documentation of the Orahi RESTful API with Swagger'
+  },
+  host: 'localhost:1337',
+  basePath: '/'
+};
+
+// options for the swagger docs
+var options = {
+  // import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // path to the API docs
+  apis: ['./routes/*.js']
+};
+
+// initialize swagger-jsdoc
+var swaggerSpec = swaggerJSDoc(options);
+
+/*
+* End of swagger spec definition
+*********************************/
 
 var app = express();
 var port = process.env.PORT || 1337;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 app.set( 'userSecret', config.userSecret ); //secret variable
 app.set( 'adminSecret', config.adminSecret ); //secret variable
 app.set( 'serviceProviderSecret', config.serviceProviderSecret ); //secret variable
@@ -38,11 +69,17 @@ require( './controllers/admin/adminController' )( app, imageDir );
 require( './controllers/readImage' )( app );
 require( './controllers/financial/financialController' )( app );
 
-
+//Server Homepage
 app.get( '/', function ( req, res )
 {
     res.send( 'Welcome to my API' );
 })
+// serve swagger spec
+app
+  .get('/swagger.json', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
 
 app.listen( port, function ()
 {
